ef6920e7b77299b8e6f6039582457b64ee0901a9
Abel Acuna <abel3121@gmail.com>
2024-02-09 21:44:19 -0500

    fix getDayAvailable

--- src/google-spreadsheet/google-spreadsheet.service.ts
+++ src/google-spreadsheet/google-spreadsheet.service.ts
@@ -43,65 +43,16 @@ export class GoogleSpreadsheetService {
       }
     }
 
-    // async getAvailableDay(): Promise<string> {
-    //   try {
-    //     await this.doc.loadInfo(); // Load the spreadsheet information
-    //     const sheet = this.doc.sheetsByIndex[0]; // Access the sheet by index
-    //     const rows:any = await sheet.getRows();
-    
-    //     const shiftsByDate = {};
-    //     rows.forEach(row => {
-    //       const [day, month, year] = row._rawData[1].split("/");
-    //       const formattedDate = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`; // Asegúrate de que el formato sea YYYY-MM-DD para ordenar
-    //       if (shiftsByDate[formattedDate]) {
-    //         shiftsByDate[formattedDate].push(row);
-    //       } else {
-    //         shiftsByDate[formattedDate] = [row];
-    //       }
-    //     });
-        
-    //     const sortedDates = Object.keys(shiftsByDate).sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
-        
-    //     // Aquí es donde se busca el primer día con menos de 4 turnos registrados.
-    //     let nextAvailableDate = sortedDates.find(date => shiftsByDate[date].length < 4);
-        
-    //     // Si no se encuentra tal día (es decir, todos los días tienen 4 turnos),
-    //     // se procede a calcular el día siguiente al último registrado.
-    //     if (!nextAvailableDate) {
-    //       const lastRegisteredDay = new Date(sortedDates[sortedDates.length - 1]);
-    //       const nextDay = new Date(lastRegisteredDay.setDate(lastRegisteredDay.getDate() + 1));
-    //       nextAvailableDate = nextDay.toLocaleDateString('es-PE', {
-    //         timeZone: 'America/Lima',
-    //       });
-    //     } else {
-    //       // Si se encuentra un día con menos de 4 turnos, se usa esa fecha directamente.
-    //       const dateObj = new Date(nextAvailableDate); // Convert the string to a Date object
-    //       console.log('dateObj', dateObj);
-    //       nextAvailableDate = dateObj.toLocaleDateString('es-PE', {
-    //         timeZone: 'America/Lima',
-    //       });
-    //     }
 
-    //     // Formatear la fecha encontrada o calculada a DD/MM/YYYY
-    //     const formattedDateDDMMYYYY = nextAvailableDate;
-    
-    //     return formattedDateDDMMYYYY;
-    //   } catch (error) {
-    //     console.error('Error retrieving the next available day:', error);
-    //     throw new Error('Failed to retrieve the next available day');
-    //   }
-    // }
-    
     async getAvailableDay(): Promise<string> {
       try {
-        await this.doc.loadInfo(); // Load the spreadsheet information
-        const sheet = this.doc.sheetsByIndex[0]; // Access the sheet by index
+        await this.doc.loadInfo();
+        const sheet = this.doc.sheetsByIndex[0];
         const rows: any = await sheet.getRows();
     
         const shiftsByDate = {};
         rows.forEach(row => {
-          // Asumiendo que row._rawData[1] contiene la fecha en formato DD/MM/YYYY
-          const fecha = row._rawData[1];
+          const fecha = row._rawData[1]; // Asume que esta es la fecha en formato DD/MM/YYYY
           if (shiftsByDate[fecha]) {
             shiftsByDate[fecha].push(row);
           } else {
@@ -109,37 +60,88 @@ export class GoogleSpreadsheetService {
           }
         });
     
-        // Convertir las fechas a objetos Date para ordenarlas y luego volver a convertirlas a DD/MM/YYYY
         const sortedDates = Object.keys(shiftsByDate).sort((a, b) => {
           const [dayA, monthA, yearA] = a.split("/").map(Number);
           const [dayB, monthB, yearB] = b.split("/").map(Number);
-          return new Date(yearA, monthA - 1, dayA).getTime() - new Date(yearB, monthB - 1, dayB).getTime();
+          return new Date(yearA, monthA - 1, dayA).valueOf() - new Date(yearB, monthB - 1, dayB).valueOf();
         });
     
-        // Buscar el primer día con menos de 4 turnos registrados
         let nextAvailableDate = sortedDates.find(date => shiftsByDate[date].length < 4);
     
-        // Si no se encuentra tal día, se calcula el día siguiente al último registrado
-        if (!nextAvailableDate) {
+        if (!nextAvailableDate && sortedDates.length > 0) {
+          // Calcula manualmente el próximo día disponible basado en la última fecha de sortedDates
           const [lastDay, lastMonth, lastYear] = sortedDates[sortedDates.length - 1].split("/").map(Number);
-          let nextDay = new Date(lastYear, lastMonth - 1, lastDay);
-          nextDay.setDate(nextDay.getDate() + 1); // Incrementa el día
-          
-          // Formatea el objeto Date a DD/MM/YYYY para la fecha del próximo día
-          nextAvailableDate = nextDay.toLocaleDateString('es-PE', {
-            timeZone: 'America/Lima',
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric',
-          });
+          let nextDay = new Date(lastYear, lastMonth - 1, lastDay + 1); // Añade un día a la última fecha registrada
+          const dd = String(nextDay.getDate()).padStart(2, '0');
+          const mm = String(nextDay.getMonth() + 1).padStart(2, '0'); //Enero es 0
+          const yyyy = nextDay.getFullYear();
+          nextAvailableDate = `${dd}/${mm}/${yyyy}`;
         }
     
-        return nextAvailableDate; // Este ya está en formato DD/MM/YYYY
+        // Si todas las fechas están llenas o no hay fechas registradas, asigna el día actual como disponible
+        if (!nextAvailableDate) {
+          const today = new Date();
+          const dd = String(today.getDate()).padStart(2, '0');
+          const mm = String(today.getMonth() + 1).padStart(2, '0'); // Enero es 0
+          const yyyy = today.getFullYear();
+          nextAvailableDate = `${dd}/${mm}/${yyyy}`;
+        }
+    
+        return nextAvailableDate;
       } catch (error) {
         console.error('Error retrieving the next available day:', error);
         throw new Error('Failed to retrieve the next available day');
       }
     }
+    
+    // async getAvailableDay(): Promise<string> {
+    //   try {
+    //     await this.doc.loadInfo(); // Load the spreadsheet information
+    //     const sheet = this.doc.sheetsByIndex[0]; // Access the sheet by index
+    //     const rows: any = await sheet.getRows();
+    
+    //     const shiftsByDate = {};
+    //     rows.forEach(row => {
+    //       // Asumiendo que row._rawData[1] contiene la fecha en formato DD/MM/YYYY
+    //       const fecha = row._rawData[1];
+    //       if (shiftsByDate[fecha]) {
+    //         shiftsByDate[fecha].push(row);
+    //       } else {
+    //         shiftsByDate[fecha] = [row];
+    //       }
+    //     });
+    
+    //     // Convertir las fechas a objetos Date para ordenarlas y luego volver a convertirlas a DD/MM/YYYY
+    //     const sortedDates = Object.keys(shiftsByDate).sort((a, b) => {
+    //       const [dayA, monthA, yearA] = a.split("/").map(Number);
+    //       const [dayB, monthB, yearB] = b.split("/").map(Number);
+    //       return new Date(yearA, monthA - 1, dayA).getTime() - new Date(yearB, monthB - 1, dayB).getTime();
+    //     });
+    
+    //     // Buscar el primer día con menos de 4 turnos registrados
+    //     let nextAvailableDate = sortedDates.find(date => shiftsByDate[date].length < 4);
+    
+    //     // Si no se encuentra tal día, se calcula el día siguiente al último registrado
+    //     if (!nextAvailableDate) {
+    //       const [lastDay, lastMonth, lastYear] = sortedDates[sortedDates.length - 1].split("/").map(Number);
+    //       let nextDay = new Date(lastYear, lastMonth - 1, lastDay);
+    //       nextDay.setDate(nextDay.getDate() + 1); // Incrementa el día
+          
+    //       // Formatea el objeto Date a DD/MM/YYYY para la fecha del próximo día
+    //       nextAvailableDate = nextDay.toLocaleDateString('es-PE', {
+    //         timeZone: 'America/Lima',
+    //         day: '2-digit',
+    //         month: '2-digit',
+    //         year: 'numeric',
+    //       });
+    //     }
+    
+    //     return nextAvailableDate; // Este ya está en formato DD/MM/YYYY
+    //   } catch (error) {
+    //     console.error('Error retrieving the next available day:', error);
+    //     throw new Error('Failed to retrieve the next available day');
+    //   }
+    // }
     
 }
 
