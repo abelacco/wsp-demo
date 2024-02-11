d5dcd709232e3a22128bc9edc78b8561306132ce
Abel Acuna <abel3121@gmail.com>
2024-02-11 18:36:58 -0500

    cierra ciclo listo para enviar lsita de aprtidas

--- src/flows/flows.service.ts
+++ src/flows/flows.service.ts
@@ -140,7 +140,7 @@ export class FlowsService {
     ctx.description = '';
     ctx.amount = 0;
     ctx.registerDate = '';
-    ctx.step = STEPS.INIT;
+    ctx.step = STEPS.NEW_EXPENSE;
     await this.senderService.sendMessages(template);
 
   }
